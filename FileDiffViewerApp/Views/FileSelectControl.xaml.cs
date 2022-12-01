89b3ea4c81e207383de8569d225fbb871a1cbb5e
Дмитрий Смирнов <painven@gmail.com>
2022-12-01 20:46:05 +0300

    Add project files.

---
+++ FileDiffViewerApp/Views/FileSelectControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FileDiffViewerApp.Views;
+/// <summary>
+/// Interaction logic for FileSelectControl.xaml
+/// </summary>
+public partial class FileSelectControl : UserControl
+{
+    public string Title
+    {
+        get { return (string)GetValue(TitleProperty); }
+        set { SetValue(TitleProperty, value); }
+    }
+
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register("Title", typeof(string), typeof(FileSelectControl), new PropertyMetadata("<Без названия>"));
+
+
+    public FileSelectControl()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+}
